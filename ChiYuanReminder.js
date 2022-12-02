332a6bca20396bbfcd856781b8c03d8b32408b07
WangKewei <wangkewei172@gmail.com>
2022-12-03 00:47:16 +0800

    Initial commit

---
+++ ChiYuanReminder.js
@@ -0,0 +1,91 @@
+/*
+ * @Author: WangKeWei172
+ * @Date: 2022/12/2
+ *
+ */
+
+import plugin from "../../lib/plugins/plugin.js";
+import common from "../../lib/common/common.js";
+import schedule from "node-schedule";
+import {Group, segment, Message} from "oicq";
+
+
+/*
+ * 使用的是cron表达式，每周六的9点和19点提醒
+ * 可以百度搜索转换器
+ */
+//const alertTime = "0 0 9,19 ? ? 7 ";
+
+const alertTime = "0 0/2 * * * ?"; //测试时间，每2分钟执行
+
+/**
+ * 开启定时推送的群号：
+ * 一个群号：
+ * groupList = ["114514"];
+ * 两个群号：
+ * groupList = ["114514","1919810"];
+ */
+const groupList = [767368570];
+
+weeklyRemindMeeting();    //开启定时提醒任务，注释掉可以关闭
+
+/**
+ * 其实我也不会写这个函数，照着众多插件的函数改的
+ * 算是勉强弄明白了使用方法
+ * 总之这是定义了一个事件（在这里为赤鸢提醒）
+ */
+export class duckImg extends plugin {
+    constructor() {
+        super({
+            name: "赤鸢提醒",
+            dsc: "根据cron表达式提醒全体成员",
+            /** https://oicqjs.github.io/oicq/#events */
+            event: "message.group",
+            priority: 1000,
+            rule: [
+                {
+                    /** 命令正则匹配 */
+                    reg: "^#?今晚8点开会$",
+                    /** 执行方法 */
+                    fnc: "remindMeeting"
+                }
+            ]
+        });
+    }
+}
+
+
+
+/**
+ * 定时提醒任务
+ *
+ */
+function weeklyRemindMeeting(){
+    schedule.scheduleJob(alertTime, () =>{
+        for(var i = 0; i < groupList.length; i++){
+            let group = Bot.pickGroup(groupList[i]);
+            remindMeeting(group);       //提醒
+            common.sleep(3000)   //休眠3s，接着发下一个群
+        }
+    })
+}
+
+/**
+ * @param e
+ * @returns {Promise<void>}
+ *
+ * 提醒开会函数
+ */
+async function remindMeeting(e){
+    let text = "今晚8点开会哟~";  /**文案*/
+    let msg = [];   //建立msg数组
+
+    if (e instanceof Group){
+        msg = [segment.at("all"), text];
+    }
+    else{
+        msg.unshift(text);
+    }
+    e.sendMsg(msg);    //发送文案
+}
+
---
+++ README.md
@@ -0,0 +1,12 @@
+# ChiYuanReminder.js-plugin
+自用[云崽](https://github.com/Le-niao/Yunzai-Bot)提醒开会插件
+
+# 功能
+利用[cron表达式](https://www.bejson.com/othertools/cron/)定时地在群里`@全体成员`提醒开会，文案、时间、群可以自定义。
+
+# 使用方法
+直接将`ChiYuanReminder.js`放到`plugins/example`文件夹里，重启云崽即可。
+
+# ToDo
+- [ ] 通过聊天框增加日程，不需要手动修改
+- [ ] 每个群有各自的日程
